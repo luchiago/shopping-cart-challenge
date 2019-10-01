cdbebe96b397b3d3866d3dd29077ce1ecb029fa1
Lucas Hiago <lucas.hiago@codeminer42.com>
2019-10-01 10:13:47 -0300

    Create Coupon controller

---
+++ app/controllers/api/v1/coupon_controller.rb
@@ -0,0 +1,36 @@
+module Api
+	module V1
+    class CartController < ApplicationController
+      def add
+        token = get_token
+        user = find_cart(token)
+        coupon = user.coupons.create(coupon_params)
+        service = CartService.new(user)
+        render json: service.calculate_discount(coupon)
+      end
+
+      def delete
+        token = get_token
+        user = find_cart(token)
+        coupon = user.coupons.find(params[:id])
+        coupon.destroy
+        redirect_to '/api/v1/cart/'
+      end
+
+      private
+      def get_token
+        request.headers['Authorization']
+      end
+
+      def coupon_params
+        params.require(:coupon).permit(:name)
+      end
+
+      def find_cart(token)
+        user = User.find_by user_token: token
+        user
+      end
+
+    end
+  end
+end
