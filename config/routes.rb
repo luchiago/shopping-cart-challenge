1bc0f895ce95486544a55c1ed142fa93418ba82a
Lucas Hiago <lucas.hiago@codeminer42.com>
2019-09-30 22:58:34 -0300

    Define routes for the controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  patch 'cart/' => 'cart#update'
+  get 'cart/' => 'cart#checkout'
+  post 'coupon/' => 'coupons#add'
+  delete 'coupon/:id' => 'coupons#delete'
 end
