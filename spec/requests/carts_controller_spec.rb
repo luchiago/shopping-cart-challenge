9e8eb2722b34f9eac5614d282823a440c5b50cd3
Lucas Hiago <lucas.hiago@codeminer42.com>
2019-10-02 01:45:06 -0300

    Refactor carts controller specs

--- spec/requests/carts_controller_spec.rb
+++ spec/requests/carts_controller_spec.rb
@@ -12,12 +12,17 @@ RSpec.describe 'PATCH cart#update', type: :request do
     }.with_indifferent_access
   end
 
-  let(:parsed) {JSON.parse(response.body)}
+  let(:parsed) { JSON.parse(response.body) }
 
-  context "when user update cart with all products" do
+  let(:headers) do
+    {
+      'Authorization' => 'teste1'
+    }
+  end
+
+  context 'when user update cart with all products' do
     before do
-      headers = { 'Authorization' => 'teste1'}
-      patch '/api/v1/cart', params: { cart: { apple: 3, banana: 1, orange: 4 } }, headers: headers
+      patch '/api/v1/cart', params: { cart: { apple: 3, banana: 1, orange: 4 } }, headers: :headers
     end
 
     it 'returns the actual cart' do
@@ -29,14 +34,13 @@ RSpec.describe 'PATCH cart#update', type: :request do
     it { is_expected.to respond_with 200 }
   end
 
-  context "when user update cart with some products" do
-    expect_values[:apple] = 0
-    expect_values[:subtotal] = 130
-    expect_values[:total] = 160
+  context 'when user update cart with some products' do
+    :expect_values[:apple] = 0
+    :expect_values[:subtotal] = 130
+    :expect_values[:total] = 160
 
     before do
-      headers = { 'Authorization' => 'teste1'}
-      patch '/api/v1/cart', params: { cart: { banana: 1, orange: 4 } }, headers: headers
+      patch '/api/v1/cart', params: { cart: { banana: 1, orange: 4 } }, headers: :headers
     end
 
     it 'returns the actual cart' do
@@ -46,14 +50,13 @@ RSpec.describe 'PATCH cart#update', type: :request do
     it { is_expected.to respond_with 200 }
   end
 
-  context "when user update cart with negative amount" do
-    expect_values[:apple] = 0
-    expect_values[:subtotal] = 130
-    expect_values[:total] = 160
+  context 'when user update cart with negative amount' do
+    :expect_values[:apple] = 0
+    :expect_values[:subtotal] = 130
+    :expect_values[:total] = 160
 
     before do
-      headers = { 'Authorization' => 'teste1'}
-      patch '/api/v1/cart', params: { cart: { apple: -1, banana: 1, orange: 4 } }, headers: headers
+      patch '/api/v1/cart', params: { cart: { apple: -1, banana: 1, orange: 4 } }, headers: :headers
     end
 
     it 'returns the actual cart' do
@@ -65,9 +68,9 @@ RSpec.describe 'PATCH cart#update', type: :request do
 end
 
 RSpec.describe 'GET cart#checkout', type: :request do
-  let(:parsed) {JSON.parse(response.body)}
+  let(:parsed) { JSON.parse(response.body) }
 
-  context "when user wants to get checkout" do
+  context 'when user wants to get checkout' do
     let(:expect_values) do
       {
         'apple' => 120,
@@ -80,8 +83,7 @@ RSpec.describe 'GET cart#checkout', type: :request do
     end
 
     before do
-      headers = { 'Authorization' => 'teste1'}
-      patch '/api/v1/cart', params: { cart: { apple: 6, banana: 6, orange: 0 } }, headers: headers
+      patch '/api/v1/cart', params: { cart: { apple: 6, banana: 6, orange: 0 } }, headers: :headers
       get '/api/v1/cart', headers: headers
     end
 
@@ -94,7 +96,7 @@ RSpec.describe 'GET cart#checkout', type: :request do
     it { is_expected.to respond_with 200 }
   end
 
-  context "when shipping is free" do
+  context 'when shipping is free' do
     let(:expect_values) do
       {
         'apple' => 420,
@@ -107,8 +109,7 @@ RSpec.describe 'GET cart#checkout', type: :request do
     end
 
     before do
-      headers = { 'Authorization' => 'teste1'}
-      patch '/api/v1/cart', params: { cart: { apple: 21 } }, headers: headers
+      patch '/api/v1/cart', params: { cart: { apple: 21 } }, headers: :headers
       get '/api/v1/cart', headers: headers
     end
 
@@ -117,7 +118,7 @@ RSpec.describe 'GET cart#checkout', type: :request do
     end
   end
 
-  context "when weight is bellow or equal 10kg" do
+  context 'when weight is bellow or equal 10kg' do
     let(:expect_values) do
       {
         'apple' => 80,
@@ -130,8 +131,7 @@ RSpec.describe 'GET cart#checkout', type: :request do
     end
 
     before do
-      headers = { 'Authorization' => 'teste1'}
-      patch '/api/v1/cart', params: { cart: { apple: 6, banana: 4} }, headers: headers
+      patch '/api/v1/cart', params: { cart: { apple: 6, banana: 4 } }, headers: :headers
       get '/api/v1/cart', headers: headers
     end
 
@@ -140,7 +140,7 @@ RSpec.describe 'GET cart#checkout', type: :request do
     end
   end
 
-  context "when weight is above 10kg" do
+  context 'when weight is above 10kg' do
     let(:expect_values) do
       {
         'apple' => 200,
@@ -153,8 +153,7 @@ RSpec.describe 'GET cart#checkout', type: :request do
     end
 
     before do
-      headers = { 'Authorization' => 'teste1'}
-      patch '/api/v1/cart', params: { cart: { apple: 10, banana: 5} }, headers: headers
+      patch '/api/v1/cart', params: { cart: { apple: 10, banana: 5 } }, headers: :headers
       get '/api/v1/cart', headers: headers
     end
 
